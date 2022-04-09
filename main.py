aaf5d44f26762b5e6c08f0fc207c675c4b40aff6
Roman Dv <romackad@gmail.com>
2022-04-09 20:45:44 +0300

    Run an app

--- main.py
+++ main.py
@@ -13,11 +13,7 @@ app = Flask(__name__)
 
 @app.route('/try')
 def FL():
-    try:
-        req = flask.Request.get_data()
-        return req
-    except:
-        return 'Hi'
+    return request
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
