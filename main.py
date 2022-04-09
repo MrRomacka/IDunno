6fbfcdcf722cfe40021cfab95b269d1deeddb488
Roman Dv <romackad@gmail.com>
2022-04-09 17:45:49 +0300

    Fix requirements

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template, url_for
 import requests
 import json
 import sqlite3
+import base64
+
+def decodeb64(coded_st):
+    base64.b64decode(coded_st)
+    return coded_st
+
 
 app = Flask(__name__)
 
 @app.route('/'):
 def FL():
-    pass
+    req = requests.get()
+    req = decodeb64(req)
+    print(req)
 
