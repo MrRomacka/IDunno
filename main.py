758eaf42cdbcc09314414de04b4186c7b57a8aaa
Roman Dv <romackad@gmail.com>
2022-04-09 20:39:44 +0300

    Run an app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, url_for, request
 import requests
 import json
@@ -8,14 +9,13 @@ def decodeb64(coded_st):
     base64.b64decode(coded_st)
     return coded_st
 
-
 app = Flask(__name__)
 
 @app.route('/try')
 def FL():
     try:
-        req1 = request.args.get()
-        print(req1)
+        req = flask.Request.get_data()
+        print(req)
     except:
         print('Hi')
 
